373d9d80bd6f36863b5ee6a9eb2c965dcefa56d6
javierrrp <javier.poblete2023@alu.uct.cl>
2024-11-08 13:38:22 -0300

    terminar codigo

--- Lab_3_Lazarus/backup/backup/unit1.pas
+++ Lab_3_Lazarus/backup/backup/unit1.pas
@@ -5,7 +5,8 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
+  ComCtrls, Buttons, TAGraph, TASeries;
 
 type
 
@@ -13,16 +14,28 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
-    procedure Button1Click(Sender: TObject);
+    Chart1: TChart;
+    Chart1BarSeries1: TBarSeries;
+    CheckBox1: TCheckBox;
+    Label1: TLabel;
+    Panel1: TPanel;
+    SpeedButton1: TSpeedButton;
+    Timer1: TTimer;
+    TrackBar1: TTrackBar;
     procedure FormCreate(Sender: TObject);
+    procedure SpeedButton1Click(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
   private
-
+    procedure CargarDatos;
   public
 
   end;
 
 var
   Form1: TForm1;
+  aData: array[1..1000] of Single;  // Almacena los datos de temperatura
+  totalDatos: Integer;              // Cantidad de datos cargados
+  indiceActual: Integer;            // Índice actual para el temporizador
 
 implementation
 
@@ -30,14 +43,58 @@ implementation
 
 { TForm1 }
 
-procedure TForm1.Button1Click(Sender: TObject);
-begin
-
-end;
-
 procedure TForm1.FormCreate(Sender: TObject);
 begin
+  // Inicializamos variables
+  totalDatos := 0;
+  indiceActual := 1;
+end;
 
+procedure TForm1.CargarDatos;
+var
+  archivo: File of Single;
+  dato: Single;
+begin
+  // Intentamos abrir el archivo binario
+  AssignFile(archivo, 'sensores.dat');
+  try
+    Reset(archivo);
+    totalDatos := 0;
+    // Leemos cada valor de temperatura y lo guardamos en el arreglo
+    while (not Eof(archivo)) and (totalDatos < Length(aData)) do
+    begin
+      Read(archivo, dato);
+      Inc(totalDatos);
+      aData[totalDatos] := dato;
+    end;
+    CloseFile(archivo);
+  except
+    ShowMessage('No se pudo abrir el archivo "sensores.dat".');
+  end;
+end;
+
+procedure TForm1.SpeedButton1Click(Sender: TObject);
+begin
+  // Cargamos los datos y reiniciamos el índice del temporizador
+  CargarDatos;
+  indiceActual := 1;
+  Chart1BarSeries1.Clear;  // Limpiamos la gráfica
+  Timer1.Enabled := True;  // Iniciamos el temporizador para actualizar el gráfico
+end;
+
+procedure TForm1.Timer1Timer(Sender: TObject);
+begin
+  if indiceActual <= totalDatos then
+  begin
+    // Agregamos el valor actual al gráfico en formato de barra
+    Chart1BarSeries1.AddY(aData[indiceActual], IntToStr(indiceActual));
+    Inc(indiceActual);  // Avanzamos al siguiente índice
+  end
+  else
+  begin
+    // Deshabilitamos el temporizador si se completaron los datos
+    Timer1.Enabled := False;
+  end;
 end;
 
 end.
--- Lab_3_Lazarus/backup/unit1.pas
+++ Lab_3_Lazarus/backup/unit1.pas
@@ -5,7 +5,8 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
+  ComCtrls, Buttons, TAGraph, TASeries;
 
 type
 
@@ -13,11 +14,23 @@ type
 
   TForm1 = class(TForm)
     Button1: TButton;
+    Chart1: TChart;
+    Chart1BarSeries1: TBarSeries;
+    CheckBox1: TCheckBox;
+    Label1: TLabel;
     Panel1: TPanel;
+    SpeedButton1: TSpeedButton;
+    Timer1: TTimer;
+    TrackBar1: TTrackBar;
     procedure Button1Click(Sender: TObject);
+    procedure CheckBox1Change(Sender: TObject);
     procedure Edit1Change(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure Label1Click(Sender: TObject);
     procedure Panel1Click(Sender: TObject);
+    procedure SpeedButton1Click(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+    procedure Timer2Timer(Sender: TObject);
   private
 
   public
@@ -26,6 +39,8 @@ type
 
 var
   Form1: TForm1;
+  aData: Array[1..1000] of Single;
+  nOffSet: Word; lOffSet: Boolean;
 
 implementation
 
@@ -38,17 +53,77 @@ begin
 
 end;
 
+procedure TForm1.CheckBox1Change(Sender: TObject);
+begin
+
+end;
+
 procedure TForm1.Edit1Change(Sender: TObject);
 begin
 
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
+Var;
+  i: Byte;
+  t: Array[1..20] of string = ['t01', 't02', 't03', 't04', 't05', 't06',
+  't07', 't08', 't09', 't10', 't11', 't12', 't13', 't14', 't15', 't16', 't17',
+  't18', 't19', 't20']
+  b: byts;
+
+
+begin
+
+end;
+
+procedure TForm1.Label1Click(Sender: TObject);
 begin
 
 end;
 
 procedure TForm1.Panel1Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.SpeedButton1Click(Sender: TObject);
+ Var
+   nPh: File of Single;
+   nData: Single;
+   nPos: Byte;
+   lErr: Boolean;
+begin
+  nPos := 1; lErr := True;
+  AssignFile(nPh, 'sensores.dat');
+  Try
+    Reset(nPh);
+    While not Eof(nPh) Do
+     Begin
+       Read(nPh, nData);
+       aData[nPos] := nData;
+       Inc(nPos);
+     end;
+    For nPos := 1 To 20 Do
+     Chart1BarSeries1.SetYValue(nPos=1, aData[nPos]);
+    CloseFile(nPh); lErr := False;
+  finally
+    if lErr Then ShowMessage('archivo no existe');
+  end;
+end;
+
+procedure TForm1.Timer1Timer(Sender: TObject);
+
+begin
+  if CheckBox1Change.Checked Then
+                 Begin
+                   dato := aData[i];
+                   For nPos := 1 to Max] Do
+                    dato[nPos] := dato[nPos = 1];
+                   dato[Max] :=
+
+end;
+
+procedure TForm1.Timer2Timer(Sender: TObject);
 begin
 
 end;
